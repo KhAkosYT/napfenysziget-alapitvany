bd873dd173b9959c1d8662addc49320516d6dec5
KhAkosYT <sedlakakos45@gmail.com>
2026-09-14 14:23:15 +0200

    publikálás utáni bugfix-ek
    a public mappa tartalmazza az összes olyan fájlt és almappát, amik publikálásra kerültek Netlify-on. Mondhatni egy teszt publikálás volt, az én emailemmel van csinálva egy felhasználó, majd meg kell csinálni az Alapítványos emailre.

---
+++ public/napfenysziget-alapitvany/scripts/hirek-data.js
@@ -0,0 +1,88 @@
+const newsData = [
+  {
+    "id": "varromuhely-2023",
+    "title": "Sikeres elindulás a megváltozott munkaképességűek foglalkoztatásában: Új varróműhelyünk története",
+    "date": "2023. ősz",
+    "shortDescription": "2023 őszén elindítottuk megváltozott munkaképességűeket foglalkoztató varróműhelyünket, ahol értékteremtő munkával, stabil megélhetéssel és elfogadó közösséggel támogatjuk munkatársainkat.",
+    "fullDescription": "A Napfény Sziget Alapítvány életében mindig is kiemelt szerepet játszott az értékteremtés és a társadalmi felelősségvállalás. Hosszas előkészítő munka után, 2023 őszén egy mérföldkőhöz érkeztünk: hivatalosan is elindítottuk a megváltozott munkaképességűek foglalkoztatását célzó programunkat, kifejezetten a varrás és textil-újrahasznosítás területén.\n\nEz a kezdeményezés nem csupán arról szól, hogy munkalehetőséget biztosítunk azok számára, akik a nyílt munkaerőpiacon hátránnyal indulnak, hanem egy olyan támogató, elfogadó közösség megteremtéséről is, ahol mindenki kibontakoztathatja a tehetségét. A műhelyben dolgozó munkatársaink precíz és odaadó munkájával nemcsak minőségi termékek készülnek, hanem valódi érték születik. Büszkék vagyunk arra, hogy stabil megélhetést, szakmai fejlődést és önbecsülést adhatunk a csapatunk tagjainak. A program hosszú távú fenntarthatóságán folyamatosan dolgozunk, hiszen látjuk, hogy a munka világa hogyan képes visszaadni a hitet és a méltóságot a mindennapokban.",
+
+    "mediaHtml": ``
+  },
+  {
+    "id": "gyermekruha-osztas-2023",
+    "title": "Mosoly az arcokon: Rendkívüli és nagyszabású gyermekruha-osztási akciónk mérlege",
+    "date": "2023. tavasz",
+    "shortDescription": "Nagyszabású gyűjtésünknek köszönhetően több száz kiváló minőségű gyermekruhát és cipőt juttattunk el Tótkomlós rászoruló családjaihoz, bölcsiseihez és óvodásaihoz.",
+    "fullDescription": "2023 tavaszán a Napfény Sziget Alapítvány csapata egy emberfeletti, mégis szívmelengető projektet valósított meg. Hónapokon át tartó gyűjtés, szelektálás és logisztikai szervezés után egy rendkívül nagyszabású gyermekruha-osztási akciót indítottunk útjára a rászoruló családok és Tótkomlós település bölcsisei és óvodásai részére váltóruha megsegítésére.\n\nTudjuk, hogy a gyorsan növekvő gyermekek ruháztatása mekkora anyagi terhet ró a nehéz sorsú szülőkre, ezért célunk az volt, hogy közvetlen és azonnali segítséget nyújtsunk. Az önkénteseink áldozatos munkájának és a nagylelkű felajánlóknak köszönhetően több száz kiváló minőségű, tiszta ruhadarabot – kabátokat, pulóvereket, nadrágokat és cipőket – sikerült eljuttatnunk a legkiszolgáltatottabb helyzetben lévő gyermekekhez. A rendezvény során nemcsak tárgyi adományokat adtunk át, hanem reményt, figyelmet és szeretetet is. Látni a gyermekek arcán megnyíló őszinte mosolyt és a szülők megkönnyebbülését volt a legnagyobb elismerés mindannyiunk számára, ami megerősített minket abban, hogy a munkánknak valós ereje van.",
+
+    "mediaHtml": ``,
+    "images": [
+      "images\\kepek\\2023_gyermekruha-osztas_1.jpg",
+      "images\\kepek\\2023_gyermekruha-osztas_2.jpg",
+    ]
+  },
+  {
+    "id": "adomanycsomag-hodmezovasarhely-2025",
+    "title": "Összefogás a legkisebbekért: Hatalmas adománycsomag Hódmezővásárhely önkormányzatának",
+    "date": "2025. ősz",
+    "shortDescription": "2025 őszén meleg gyermekruhákból, lábbelikből és fejlesztő játékokból álló hatalmas adománycsomagot adtunk át Hódmezővásárhely Önkormányzatának a helyi rászoruló gyermekek megsegítésére.",
+    "fullDescription": "A Napfény Sziget Alapítvány küldetése, hogy a segítség ott érjen célba, ahol a legnagyobb szükség van rá. Ennek szellemében 2025 őszén egy kiemelkedő jelentőségű, nagyszabású jótékonysági akciót szerveztünk, melynek keretében Hódmezővásárhely Megyei Jogú Város Önkormányzata részére ajánlottunk fel egy hatalmas mennyiségű adománycsomagot.\n\nA szállítmány rengeteg meleg gyermekruhát, lábbelit, valamint készségfejlesztő és szórakoztató gyermekjátékokat tartalmazott, melyeket kifejezetten a településen élő, hátrányos helyzetű és rászoruló gyermekek számára juttattunk el. Az önkormányzattal való szoros együttműködés garantálta, hogy az adományok a legmegfelelőbb helyekre, közvetlenül a krízishelyzetben lévő családokhoz kerüljenek. Hiszünk abban, hogy a játékhoz és a megfelelő ruházathoz való jog minden gyermeket megillet, függetlenül a családja anyagi helyzetétől. Ez a sikeres felajánlás kiváló példája annak, hogy a civil szféra és a helyi önkormányzatok összefogásával mekkora változást lehet elérni a helyi közösségek életében.",
+
+    "mediaHtml": `
+      <div class="nap-news-media-box">
+        <h5><i class="fa-brands fa-facebook text-primary"></i> Kapcsolódó videó és sajtómegjelenés</h5>
+        
+        <!-- Facebook Videó iframe -->
+        <div class="ratio ratio-16x9 mb-3">
+          <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fvtvvasarhely%2Fvideos%2F1327761338554414%2F&show_text=false&width=560&t=0" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+        </div>
+
+        <!-- Újságcikk link -->
+        <a href="https://www.hodpress.hu/amikor-a-legkisebbek-tanitanak-minket-adni/" target="_blank" class="btn btn-outline-secondary btn-sm">
+          <i class="fa-solid fa-newspaper"></i> Olvassa el a cikket a Hódpress.hu oldalon!
+        </a>
+      </div>
+    `,
+
+    "images": [
+      "images\\kepek\\2025_adomanycsomag-hodmezovasarhely_1.jpeg",
+      "images\\kepek\\2025_adomanycsomag-hodmezovasarhely_2.jpeg",
+      "images\\kepek\\2025_adomanycsomag-hodmezovasarhely_3.jpeg",
+      "images\\kepek\\2025_adomanycsomag-hodmezovasarhely_4.jpeg",
+
+    ]
+  },
+  {
+    "id": "iskolakezdes-tanszer-2026",
+    "title": "Egyenesen az iskolapadba: Teljes tanszerfelszereléssel és megtöltött iskolatáskákkal támogattuk a kis elsősöket (2026.08.25-én)",
+    "date": "2026.08.25.",
+    "shortDescription": "Teljesen felszerelt, ergonomikus iskolatáskákkal és teljes tornafelszereléssel léptettük át az iskolapad küszöbét két rászoruló kis elsősnél, megkönnyítve a tanévkezdést.",
+    "fullDescription": "A tanévkezdés minden család életében hatalmas mérföldkő, az iskolai felszerelések beszerzése azonban komoly anyagi terhet ró a nehezebb sorsú szülőkre. A Napfény Sziget Alapítvány éppen ezért tartja kiemelten fontosnak, hogy a legkisebbek egyenlő esélyekkel, örömmel és magabiztosan indulhassanak el a tudás útján. Tegnapi napon egy különleges és szívmelengető átadáson vehettünk részt, ahol két tüneményes gyermek, egy kisfiú és egy kislány arcára varázsolhattunk hatalmas mosolyt. Mindketten idén, szeptember 1-jén kezdik meg az első osztályt, ami az életük egyik legizgalmasabb időszakának kezdete.\n\nAlapítványunk jóvoltából a két kis diák nem üres kézzel vág neki az iskolának: mindketten egy-egy teljesen megtöltött, ergonomikus és modern iskolatáskát kaptak. A táskákba minden szükséges kiegészítő kellék bekerült, a füzetektől kezdve a színes ceruzákon, tolltartókon, vonalzókon át egészen a kreatív alkotómunkához szükséges rajzeszközökig. Emellett a testnevelésórákra is gondoltunk, így a teljes tornafelszerelést – tornazsákot és a sportoláshoz szükséges ruházatot – is biztosítottuk számukra. Látni a gyermekek csillogó szemét és a szülők megkönnyebbülését, amikor átvették a csomagokat, leírhatatlan érzés volt. Hiszünk abban, hogy ezzel a támogatással nemcsak eszközöket adtunk, hanem egy gondtalanabb, boldogabb tanévkezdést is, amely megalapozza a jövőbeli sikereiket az iskolapadban.",
+
+    "mediaHtml": `
+      <div class="nap-news-media-box">
+        <h5><i class="fa-brands fa-facebook text-primary"></i> Kapcsolódó videó és sajtómegjelenés</h5>
+        
+        <!-- Facebook Videó iframe -->
+        <div class="ratio ratio-16x9 mb-3">
+          <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F2373012556850149%2F&show_text=false&width=560&t=0" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+        </div>
+
+        <!-- Facebook cikk link -->
+        <div class="ratio ratio-16x9 mb-3">
+          <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fgyorshirszolgalat%2Fposts%2Fpfbid024uZiTP7PgnKS5DkiRAgJQ2ZQcJSychgGXuP7ZQdhP1jHtCSTMhyMZoGoY5RKgNhrl&show_text=true&width=500" width="500" height="896" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        </div>
+
+        <!-- Újságcikk link -->
+        <a href="https://www.hodpress.hu/gyermekek-iskolakezdeset-konnyitettek-meg-a-tanszerosztassal/" target="_blank" class="btn btn-outline-secondary btn-sm">
+          <i class="fa-solid fa-newspaper"></i> Olvassa el a cikket a Hódpress.hu oldalon!
+        </a>
+      </div>
+    `,
+
+    "images": [
+      "images\\kepek\\2026_iskolatanszer_adomanyozas_1.jpg",
+      "images\\kepek\\2026_iskolatanszer_adomanyozas_2.jpg",
+    ]
+  }
+];
